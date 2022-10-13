faf33921c7f290063bf03dfc853c0549ee2625bb
JavonPeart <44687372+JavonPeart@users.noreply.github.com>
2022-10-13 17:45:58 -0500

    Exercise 4 - Check for winner and update status

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,18 +1,22 @@
 window.onload = function(){
     var boardLayout = document.querySelectorAll("#board > div");
+    var status = document.getElementById("status");
 
     var playerX = "X";
     var playerO = "O";
     var currplayer = "X";
 
+    var winCondition = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+
     for(let i=0; i<=8; i++){
         boardLayout[i].setAttribute("class","square");
         boardLayout[i].innerHTML = "";
-    }
+    };
 
     boardLayout.forEach((element) =>{
         element.classList.add("square");
         element.innerHTML = "";
+
 
         element.addEventListener("click", function(){
             if ((currplayer == "O") && (element.innerHTML == "")){
@@ -20,11 +24,28 @@ window.onload = function(){
                 element.classList.add("X");
                 currplayer = "X";
 
-            }else if ((currplayer == "X") && (element.innerHTML =="")){
+            }else if ((currplayer == "X") && (element.innerHTML == "")){
                 element.innerHTML = playerO;
                 element.classList.add("O");
                 currplayer = "O";
             }
+
+            winCondition.forEach((index)=> {
+                if((boardLayout[index[0]].innerHTML == playerX) 
+                && (boardLayout[index[1]].innerHTML == playerX) 
+                && (boardLayout[index[2]].innerHTML == playerX)){
+                    status.classList.add("you-won");
+                    status.innerHTML = ("Congratulations! X is the Winner!");
+                    
+                }else if((boardLayout[index[0]].innerHTML == playerO) 
+                && (boardLayout[index[1]].innerHTML == playerO) 
+                && (boardLayout[index[2]].innerHTML == playerO)){
+                    status.classList.add("you-won");
+                    status.innerHTML = ("Congratulations! O is the Winner!");
+                }
+            });
+    
+            
         });
         
         element.onmouseover = (event) =>{
